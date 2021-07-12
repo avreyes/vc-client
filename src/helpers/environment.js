2dc8fcacc802e00d63b2e36762c8d6127c2fba51
avreyes <alyssa.va.reyes@gmail.com>
2021-07-12 02:38:25 -0400

    need debug in login

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,8 +4,8 @@ switch (window.location.hostname) {
     case 'localhost' || '127.0.0.1':
         APIURL = 'http://localhost:3050';
         break;
-    case 'avr-vibecheck-client.herokuapp.com':
-        APIURL = 'https://avr-vibecheck-client.herokuapp.com'
+    case 'avr-vibecheck.herokuapp.com':
+        APIURL = 'https://avr-vibecheck.herokuapp.com'
 }
 
 export default APIURL;
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
 
 const Login = (props) => {
-    const [username, setUsername] = useState('');
+    console.log(props);
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     
 
@@ -12,7 +13,7 @@ const Login = (props) => {
         fetch('http://localhost:3050/user/login', {
             method: 'POST',
             body: JSON.stringify(
-                {user:{username: username, password: password}}
+                {user:{email: email, password: password}}
             ),
             headers: new Headers ({
                 'Content-Type': 'application/json/'
@@ -24,13 +25,16 @@ const Login = (props) => {
         })
     }
 
+                //// where to put console.log(token) to see in console ////
+                //// need to clear session Token ///
+
     return (
         <div>
             <h1>Login</h1>
             <Form onSubmit={handleSubmit}>
                 <FormGroup>
-                    <Label htmlFor='username'>Username</Label>
-                    <Input onChange={(e) => setUsername(e.target.value)} name='username' value={ username } />
+                    <Label htmlFor='email'>Email</Label>
+                    <Input onChange={(e) => setEmail(e.target.value)} name='email' value={ email } />
                 </FormGroup>
                 <FormGroup>
                     <Label htmlFor='password'>Password</Label>
